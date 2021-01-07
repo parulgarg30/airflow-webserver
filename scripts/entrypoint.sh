89fd95ec605ef09a2b24c26f622a47dfc4102f1d
parulgarg30 <77074876+parulgarg30@users.noreply.github.com>
2021-01-07 22:46:00 +0530

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-airflow db init
+airflow db init && airflow users create --firstname admin --lastname admin --email admin --password admin --username admin --role Admin
 
 exec airflow webserver
